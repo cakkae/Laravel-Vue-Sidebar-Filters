59374a1726b20662e0ec83804e50953bf9d26b23
Adnan Beganović <cakkae@hotmail.com>
2021-01-28 21:47:50 +0100

    Update

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -2,52 +2,52 @@
 namespace App\Http\Controllers\Api;
 
 use Auth;
-use App\Product;
 use App\User;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\CartResource;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
+
+    public function __construct()
+    {
+        $user_id = 1;
+        $this->user = User::where('id',$user_id)->get()->first();
+    }
+
     public function cart()  {
-        $user = User::where('id',1);
-        return CartResource($this->user);
+        return \Cart::session($this->user->id)->getContent();
     }
 
     public function add(Request $request){
-        $user = User::where('id',1)->get();
-        \Cart::session($user->first()->id)->add(array(
+        \Cart::session($this->user->id)->add(array(
             'id' => '1',
             'name' => 'TEst',
             'price' => 55,
             'quantity' => 44,
         ));
-        return CartResource::collection($user);
+        return \Cart::session($this->user->id)->getContent();
     }
 
     public function remove(Request $request){
-        \Cart::remove($request->id);
-        $cartCollection = \Cart::getContent();
-        return CartResource::collection($cartCollection);
+        \Cart::session($this->user->id)->remove($request->id);
+        return \Cart::session($this->user->id)->getContent();
     }
 
     public function update(Request $request){
-        \Cart::update($request->id,
+        \Cart::session($this->user->id)::update($request->id,
             array(
                 'quantity' => array(
                     'relative' => false,
                     'value' => $request->quantity
                 ),
         ));
-        $cartCollection = \Cart::getContent();
-        return CartResource::collection($cartCollection);
+        return \Cart::session($this->user->id)->getContent();
     }
 
     public function clear(){
-        \Cart::clear();
-        $cartCollection = \Cart::getContent();
-        return CartResource::collection($cartCollection);
+        \Cart::session($this->user->id)->clear();
+        return \Cart::session($this->user->id)->getContent();
     }
 
 }
