dd43d719515c7a700a6d21369c89c5479590f203
Adnan Beganović <cakkae@hotmail.com>
2021-02-01 15:58:45 +0100

    Added API

---
+++ API/dbconfig.js
@@ -0,0 +1,15 @@
+
+const config = {
+    user :'BestWeb',
+    password :'Ulaz123#321!',
+    server:'sarajevo.publicvm.com',
+    database:'Proksi',
+    options:{
+        trustedconnection: true,
+        enableArithAbort : true, 
+        instancename :'SQLEXPRESS'
+    },
+    port : 1436
+}
+
+module.exports = config; 
